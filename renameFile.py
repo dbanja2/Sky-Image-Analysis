fa9f771ae85537018aa0c66adaea07d9b8b52d76
dbanja2 <49877837+dbanja2@users.noreply.github.com>
2020-08-26 13:12:46 -0500

    Update renameFile.py

--- renameFile.py
+++ renameFile.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 17 15:47:07 2019
 This code will rename any filename to list of numbers from 0
 @author: dbanja2
 """
 
 import os
 os.getcwd()
-collection = "C:/Users/dbanja2/Downloads/SkyImages/Cloudy/"
+collection = "C:/Users/username/yourSkyImagesLocation/SkyImages/Cloudy/"
 for i, filename in enumerate(os.listdir(collection)):
-    os.rename("C:/Users/dbanja2/Downloads/SkyImages/Cloudy/" + filename, "C:/Users/dbanja2/Downloads/SkyImages/Cloudy/" + str(i) + ".jpg")
+    os.rename("C:/Users/username/yourSkyImagesLocation/SkyImages/Cloudy/" + filename, "C:/Users/username/yourSkyImagesLocation/SkyImages/Cloudy/" + str(i) + ".jpg")
